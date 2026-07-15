e6911445643f9d78f8018bdb027d082de8861171
bhoomikaupadhyaya <bhoomikaupadhyaya@gmail.com>
2026-07-15 12:07:09 +0530

    updated

---
+++ Frontend/src/routs.jsx
@@ -0,0 +1,50 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Upload from "./pages/Upload";
+import VideoDetails from "./pages/VideoDetails";
+import Notes from "./pages/Notes";
+import Quiz from "./pages/Quiz";
+import Flashcards from "./pages/Flashcards";
+import Chat from "./pages/Chat";
+import History from "./pages/History";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route path="/upload" element={<Upload />} />
+
+        <Route path="/video/:id" element={<VideoDetails />} />
+
+        <Route path="/notes/:id" element={<Notes />} />
+
+        <Route path="/quiz/:id" element={<Quiz />} />
+
+        <Route path="/flashcards/:id" element={<Flashcards />} />
+
+        <Route path="/chat/:id" element={<Chat />} />
+
+        <Route path="/history" element={<History />} />
+
+        <Route path="/profile" element={<Profile />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
