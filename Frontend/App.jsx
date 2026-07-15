1b18926c33a0284317ad9199d58bbd50cda5617e
bhoomikaupadhyaya <bhoomikaupadhyaya@gmail.com>
2026-07-15 11:52:15 +0530

    updated

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -1,11 +1,7 @@
+import AppRoutes from "./routes/AppRoutes";
+
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
-      <h1 className="text-4xl font-bold">
-        VideoMind AI
-      </h1>
-    </div>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
---
+++ Frontend/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="min-h-screen flex justify-center items-center text-white">
+      Home Page
+    </div>
+  );
+}
+
+export default Home;
