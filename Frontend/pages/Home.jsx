d68c1b51498de3799c28361c711508b9af2a2db0
bhoomikaupadhyaya <bhoomikaupadhyaya@gmail.com>
2026-07-15 12:27:57 +0530

    updated

--- Frontend/pages/Home.jsx
+++ Frontend/pages/Home.jsx
@@ -1,7 +1,34 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+import HeroSection from "../components/HeroSection";
+import FeaturesSection from "../components/FeaturesSection";
+import HowItWorks from "../components/HowItWorks";
+import WhyChoose from "../components/WhyChoose";
+import CTASection from "../components/CTASection";
+
 function Home() {
   return (
-    <div className="min-h-screen flex justify-center items-center text-white">
-      Home Page
+    <div className="bg-slate-900 text-white overflow-x-hidden">
+
+      <Navbar />
+
+      <main>
+
+        <HeroSection />
+
+        <FeaturesSection />
+
+        <HowItWorks />
+
+        <WhyChoose />
+
+        <CTASection />
+
+      </main>
+
+      <Footer />
+
     </div>
   );
 }
