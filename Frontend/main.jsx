fa2ba2ab771ea5125c96dea243641564259284e8
bhoomikaupadhyaya <bhoomikaupadhyaya@gmail.com>
2026-07-22 12:03:38 +0530

    updated

--- Frontend/main.jsx
+++ Frontend/main.jsx
@@ -8,10 +8,15 @@ import "./index.css";
 
 import "react-toastify/dist/ReactToastify.css";
 
+import { AuthProvider } from "./context/AuthContext";
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
